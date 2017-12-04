dd4809eb77572df4baa04eb1d87a0f51f72cb865
andfwas <andfwas@gmail.com>
2017-12-04 10:49:17 -0700

    playing with viewport resize

--- js/viewport.js
+++ js/viewport.js
@@ -3,7 +3,7 @@ var scene = new THREE.Scene()
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
 
 var renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerWidth)
+renderer.setSize(window.innerWidth, window.innerHeight)
 
 document.body.appendChild(renderer.domElement)
 window.addEventListener('resize', () => {
@@ -33,10 +33,10 @@ var render = function() {
 }
 
 var GameLoop = function() {
-  requestAnimationFrame(GameLoop)
 
   update()
   render()
+  requestAnimationFrame(GameLoop)
 }
 
 GameLoop()
