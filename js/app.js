7025fc4e01b1eba0242fea9117560f6639ac0eaf
andfwas <andfwas@gmail.com>
2017-12-04 13:28:23 -0700

    shooter shape OK, mouse track OK

--- js/app.js
+++ js/app.js
@@ -23,15 +23,18 @@ const sphere1 = new THREE.SphereGeometry(75, 100, 100);
 const sphere2 = new THREE.SphereGeometry(75, 100, 100);
 const sphere3 = new THREE.SphereGeometry(75, 100, 100);
 const sphere4 = new THREE.SphereGeometry(75, 100, 100);
+const shooter = new THREE.ConeGeometry(20, 100, 3)
 
 const material1 = new THREE.MeshLambertMaterial({color: 0x00FF00});
 const material2 = new THREE.MeshLambertMaterial({color: 0x0000FF});
 const material3 = new THREE.MeshLambertMaterial({color: 0xFF0000});
 const material4 = new THREE.MeshLambertMaterial({color: 0xFFFF00});
+const material5 = new THREE.MeshLambertMaterial({color: 0xFF00FF});
 const mesh1 = new THREE.Mesh(sphere1, material1);
 const mesh2 = new THREE.Mesh(sphere2, material2);
 const mesh3 = new THREE.Mesh(sphere3, material3);
 const mesh4 = new THREE.Mesh(sphere4, material4);
+const mesh5 = new THREE.Mesh(shooter, material5);
 
 mesh1.position.x = -500;
 mesh1.position.y = 50;
@@ -49,10 +52,30 @@ mesh4.position.x = 500;
 mesh4.position.y = 50;
 mesh4.position.z = -1000;
 
+mesh5.position.x = 0;
+mesh5.position.y = -75;
+mesh5.position.z = -500;
+
+mesh5.rotation.x = 75;
+mesh5.rotation.y = 0;
+mesh5.rotation.z = 0;
+
 scene.add(mesh1);
 scene.add(mesh2);
 scene.add(mesh3);
 scene.add(mesh4);
+scene.add(mesh5);
+
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
+
+function onMouseMove( event ) {
+	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+  // console.log(mouse.x*500);
+  mesh5.rotation.z = -mouse.x*1.2;
+}
+window.addEventListener( 'mousemove', onMouseMove, false );
 
 requestAnimationFrame(render);
 
